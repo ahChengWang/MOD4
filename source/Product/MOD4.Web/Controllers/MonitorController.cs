0daaf31693449f1593df47cf77f7e017af9bd806
wang <wang78906@yahoo.com.tw>
2023-05-26 17:00:04 +0800

    modify project and add Monitor page initial

---
+++ source/Product/MOD4.Web/Controllers/MonitorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MOD4.Web.DomainService;
+
+namespace MOD4.Web.Controllers
+{
+    [Authorize]
+    public class MonitorController : BaseController
+    {
+        private readonly ILogger<ExtensionController> _logger;
+
+        public MonitorController(IHttpContextAccessor httpContextAccessor,
+            IAccountDomainService accountDomainService,
+            ILogger<ExtensionController> logger)
+            : base(httpContextAccessor, accountDomainService)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
